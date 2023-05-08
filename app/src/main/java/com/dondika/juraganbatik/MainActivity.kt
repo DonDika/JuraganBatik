d5851b2a21bc6ff02e0fc40f5ff953aac3962c33
IkhsanMardika <ikhsancahyamardika@gmail.com>
2023-05-08 07:45:03 +0700

    feat: Add bottom navigation

--- app/src/main/java/com/dondika/juraganbatik/MainActivity.kt
+++ app/src/main/java/com/dondika/juraganbatik/MainActivity.kt
@@ -2,10 +2,32 @@ package com.dondika.juraganbatik
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.dondika.juraganbatik.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupBottomNav()
     }
+
+    private fun setupBottomNav() {
+        val bottomNavView: BottomNavigationView = binding.bottomNav
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavView.setupWithNavController(navController)
+    }
+
 }
