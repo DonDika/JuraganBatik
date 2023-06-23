af2ced6e15e14744cab185cfc09042ef231beae9
IkhsanMardika <ikhsancahyamardika@gmail.com>
2023-06-23 14:47:15 +0700

    feat: Add layout for user

--- app/src/main/java/com/dondika/juraganbatik/ui/user/MainActivityUser.kt
+++ app/src/main/java/com/dondika/juraganbatik/ui/user/MainActivityUser.kt
@@ -2,11 +2,31 @@ package com.dondika.juraganbatik.ui.user
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.dondika.juraganbatik.R
+import com.dondika.juraganbatik.databinding.ActivityMainUserBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivityUser : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainUserBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_user)
+        binding = ActivityMainUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupBottomNav()
     }
+
+    private fun setupBottomNav() {
+        val bottomNavView: BottomNavigationView = binding.bottomNavUser
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostUserFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavView.setupWithNavController(navController)
+
+    }
+
+
 }
